b97b0b9dafa696924389deab2ada57a6f40d7205
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-09-11 16:49:51 -0300

    agrego los estilos para las banderas y el boton de next

--- src/screens/Game/index.js
+++ src/screens/Game/index.js
@@ -4,28 +4,33 @@ import styles from './index.module.scss';
 
 export default function Game(){
   return(
-    <div className={styles.container}>
-      <div className={styles.containerCard}>
+    <div className={styles.game}>
+      <div className={styles.container}>
         <h1 className={styles.title}>Country Quiz</h1>
-        <div className={styles.card}>
+        <div className={styles.containerCard}>
           <img className={styles.imgWorld} src={imageWorld} />
-          <h2 className={styles.titleQuiz}>Kuala Lumpur is the capital of</h2>
-          <div className={styles.containerOptions}>
-             <button className={styles.button} type="button"> 
-              A  <span className={styles.optionText}> Vietam </span>
-            </button>
-             <button className={styles.button} type="button">
-               B  <span  className={styles.optionText} > vietam</span> 
-            </button>
-            <button className={styles.buttonSuccess} type="button"> 
-              C <span className={styles.optionText}>vietam</span>
-            </button>
-            <button className={styles.buttonFail} type="button"> 
-              D <span className={styles.optionText}>vietam</span>
-            </button>
+          <div className={styles.card}>
+            <img className={styles.flagCountry} src="https://restcountries.eu/data/ala.svg" alt="" />
+            <h2 className={styles.titleQuiz}>Which country does this flag belong to?</h2>
+            <div className={styles.containerOptions}>
+               <button className={styles.button} type="button"> 
+                A  <span className={styles.optionText}> Vietam </span>
+              </button>
+               <button className={styles.button} type="button">
+                 B  <span  className={styles.optionText} > vietam</span> 
+              </button>
+              <button className={styles.buttonSuccess} type="button"> 
+                C <span className={styles.optionText}>vietam</span>
+              </button>
+              <button className={styles.buttonFail} type="button"> 
+                D <span className={styles.optionText}>vietam</span>
+              </button>
+            </div>
           </div>
+          <button className={styles.buttonNext}>Next</button>
         </div>
       </div>
+      <p className={styles.nameCopyRight}>Ezequiel Aragón @ DevChallenges.io</p>
     </div>
   )
 }
