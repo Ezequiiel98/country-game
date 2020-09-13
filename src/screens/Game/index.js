d7239fe193255f9a7283dae7ec54f80c9fb85d80
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-09-13 01:52:43 -0300

    agrego peticion en game para traer los paises de la api

--- src/screens/Game/index.js
+++ src/screens/Game/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CardGame from 'components/CardGame';
 import CardFinishGame from 'components/CardFinishGame';
+import fetchCountries from 'services/fetchCountries.js';
 
 import styles from './index.module.scss';
 
 export default function Game() {
+  const [contries, setCountries] = useState([]);
+
+
+  useEffect(() => {
+    const getCountries = async () => {
+      const res = await fetchCountries();
+      setCountries(res.data);
+    };
+
+    getCountries();
+  }, []);
+
   return (
     <div className={styles.game}>
       <div className={styles.container}>
