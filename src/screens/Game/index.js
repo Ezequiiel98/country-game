3d676c73a5eaaacf9a8511804f39686edb0e5a4d
Ezequiiel98 <aragon.ezequiel98@gmail.com>
2020-09-13 04:25:37 -0300

    agrego funcion para mezclar las opciones

--- src/screens/Game/index.js
+++ src/screens/Game/index.js
@@ -5,45 +5,76 @@ import fetchCountries from 'services/fetchCountries.js';
 
 import styles from './index.module.scss';
 
+const shuffleArray = (list = []) => {
+  console.log('entra', list);
+  for (let i = 0; i < list.length; i++) {
+    const indexRandom = Math.floor(Math.random() * (list.length - 1)) + 1;
+    const itemTemp = list[i];
+
+    list[i] = list[indexRandom];
+    list[indexRandom] = itemTemp;
+  }
+
+  return list;
+};
+
 export default function Game() {
-  const [contries, setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
   const [randomCountry, setRandomCountry] = useState(null);
   const [options, setOptions] = useState([]);
+  const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
     const getCountries = async () => {
       const res = await fetchCountries();
       setCountries(res.data);
+      setLoaded(true);
     };
 
     getCountries();
   }, []);
 
   const getRandomCountry = useCallback(() => {
-    const indexRandom = Math.floor(Math.random() * contries.length + 1);
-    const country = contries[indexRandom];
+    const indexRandom = Math.floor(Math.random() * (countries.length - 1)) + 1;
+    const country = countries[indexRandom];
     setRandomCountry(country);
-  }, [contries, setRandomCountry]);
+  }, [countries, setRandomCountry]);
 
   const getOptions = useCallback(() => {
     const NUM_OPTIONS = 3;
-    const optionsAnswers = [];
-
+    const indexUsed = [];
+    const optionsAnswers = [randomCountry.name];
+    
     for (let i = 0; i < NUM_OPTIONS; i++) {
-      const indexRandom = Math.floor(Math.random() * contries.length + 1);
+      const indexRandom = Math.floor(Math.random() * (countries.length - 1)) + 1;
 
-      if (i === 0) {
-        optionsAnswers.push(randomCountry);
+      if (countries[indexRandom] === randomCountry || indexUsed.includes(indexRandom)) {
+        const index =
+          indexRandom >= 1 && indexRandom <= countries.length - 1 ? indexRandom + 1 : indexRandom - 1;
+
+        indexUsed.push(index);
+        optionsAnswers.push(countries[index].name);
+      } else {
+        optionsAnswers.push(countries[indexRandom].name);
+        indexUsed.push(indexRandom);
       }
-
-      optionsAnswers.push(contries[indexRandom]);
     }
-
+    console.log(optionsAnswers);
+    console.log('sale', shuffleArray(optionsAnswers));
     setOptions(optionsAnswers);
-  }, [randomCountry, setOptions, contries]);
+  }, [randomCountry, setOptions, countries]);
 
-  useEffect(() => getRandomCountry(), [contries, getRandomCountry]);
-  useEffect(() => getOptions(), [contries, getOptions]);
+  useEffect(() => {
+    if (loaded) {
+      getRandomCountry();
+    }
+  }, [loaded, countries, getRandomCountry]);
+
+  useEffect(() => {
+    if (loaded && randomCountry) {
+      getOptions();
+    }
+  }, [loaded, randomCountry, countries, getOptions]);
 
   return (
     <div className={styles.game}>
